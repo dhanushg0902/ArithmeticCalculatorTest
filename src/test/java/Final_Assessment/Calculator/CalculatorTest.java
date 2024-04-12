055bffdd1a6031e68db4bd1e856f7abba79ff737
dhanushg0902 <130021630+dhanushg0902@users.noreply.github.com>
2024-04-12 18:25:35 +0530

    Add files via upload

---
+++ src/test/java/Final_Assessment/Calculator/CalculatorTest.java
@@ -0,0 +1,28 @@
+package Final_Assessment.Calculator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+    
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(calculator.add(2, 3), 5);
+    }
+    
+    @Test
+    public void testSubtract() {
+        Assert.assertEquals(calculator.subtract(5, 3), 2);
+    }
+    
+    @Test
+    public void testMultiply() {
+        Assert.assertEquals(calculator.multiply(2, 3), 6);
+    }
+    
+    @Test
+    public void testDivide() {
+        Assert.assertEquals(calculator.divide(6, 3), 2.0);
+    }
+}
